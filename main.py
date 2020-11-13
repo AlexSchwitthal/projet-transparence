94c4fda96bb4dfd1880006ac7ae5ea3b77159b00
alexSchwitthal <schwitthal.alexandre@gmail.com>
2020-11-13 14:09:55 +0100

    analyse globale electre fix

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from methods import *
 from read_write_db import *
 from profils import *
 
+
 #ordonné de critère 1 à critère 6
 list_poids = [1,1,1,1,2,2]
 #ordonné de profil b6 à profil b1 et de critère 1 à critère 6
@@ -20,6 +21,7 @@ list_profils = [[100,0,0,0,100,100],
                 [1750,17,1.7,0.5,4,5],
                 [1850,20,4,0.6,3,2.5],
                 [10000,100,100,100,0,0]]
+
 
 seuil1 = 0.7
 list_cereals, list_cereals_labels = readDB1()  
@@ -83,7 +85,6 @@ defineElectreScore(list_items3, list_poids, list_profils, seuil3, label_DB3)
 #print("\n\n -- OPTIMIST --\n")
 #compareElectreNutriscore(list_items2, 'optimist_score')
 
-
 #db = 1
 print("ANALYSE ELECTRE :")     
 for db in range(1, 4):
@@ -91,7 +92,8 @@ for db in range(1, 4):
     seuil = 0.5
     for i in range(3):
         seuil = 0.5 + i*0.1
-        list_items, label_DB
+        list_items = "" 
+        label_DB = ""
         list_profils_to_use = ""
         db_to_use = ""
         if(db == 1):
@@ -99,64 +101,61 @@ for db in range(1, 4):
             list_profils_to_use = list_profilsDB1
             db_to_use = "DB1"
         elif(db == 2):
-            list_items, label_DB == readDB2()
+            list_items, label_DB = readDB2()
             list_profils_to_use = list_profilsDB2
             db_to_use = "DB2"
         elif(db == 3):
             list_items,label_DB = readDB3()
             list_profils_to_use = list_profilsDB3
             db_to_use = "DB3"
+        else:
+            print("erreur")
+            break
             
+        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils nutriscore")
         list_profils = defineProfileWithNutriscore(list_items) 
         defineElectreScore(list_items, list_poids, list_profils, seuil, label_DB)
-        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils nutriscore")
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
         analyseElectre(list_items, db_to_use, 'optimist_score')
         
-        list_items, label_DB = readDB3()
-        defineElectreScore(list_items, list_poids, list_profilsAll, seuil, label_DB)
-        defineFeuxTricolore(list_items, label_DB)  
+        
         print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils all")
+        defineElectreScore(list_items, list_poids, list_profilsAll, seuil, label_DB)
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
         analyseElectre(list_items, db_to_use, 'optimist_score')
             
-        list_items, label_DB = readDB3()
-        defineElectreScore(list_items, list_poids, list_profils_to_use, seuil, label_DB)
-        defineFeuxTricolore(list_items, label_DB)  
+        
         print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils DB", db)
+        defineElectreScore(list_items, list_poids, list_profils_to_use, seuil, label_DB)
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
         analyseElectre(list_items, db_to_use, 'optimist_score')
             
         #avec poids favorisant critères négatifs
+        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids négatif, profils nutriscore")
         list_profils = defineProfileWithNutriscore(list_items) 
-        list_items, label_DB = readDB3()
         defineElectreScore(list_items, list_poids1, list_profils, seuil, label_DB)
-        defineFeuxTricolore(list_items, label_DB)   
-        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils nutriscore")
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
         analyseElectre(list_items, db_to_use, 'optimist_score')
     
-        list_items, label_DB = readDB3()
-        defineElectreScore(list_items, list_poids1, list_profilsAll, seuil, label_DB)
-        defineFeuxTricolore(list_items, label_DB)         
-        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils all")
+
+        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids négatif, profils all")
+        defineElectreScore(list_items, list_poids1, list_profilsAll, seuil, label_DB)     
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
         analyseElectre(list_items, db_to_use, 'optimist_score')
         
-        list_items, label_DB = readDB3()
-        defineElectreScore(list_items, list_poids1, list_profils_to_use, seuil, label_DB)
-        defineFeuxTricolore(list_items, label_DB)         
-        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids de base, profils nutriscore")
+        
+        print("\n\n\nDB", db, ": seuil de ", seuil, ", poids négatif, profils DB", db)
+        defineElectreScore(list_items, list_poids1, list_profils_to_use, seuil, label_DB)  
         print("\n\n -- PESSIMIST --")
         analyseElectre(list_items, db_to_use, 'pessimist_score')
         print("\n\n -- OPTIMIST --")
